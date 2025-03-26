ec9af1ed399b1292930527e25e01ee5ca0babe19
Rizki <rizkidliyaulhaq22@gmail.com>
2025-03-26 12:23:20 +0700

    add env and change db connect to env

--- golang-crud-clean-arch/config/db.go
+++ golang-crud-clean-arch/config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -17,12 +18,20 @@ func MongoConnect() *mongo.Client {
 		return mongoClient // Gunakan koneksi yang sudah ada
 	}
 
+	mongoURI := os.Getenv("MONGO_URI")
+	mongoDBName := os.Getenv("MONGO_DB_NAME")
+
+	if mongoURI == "" || mongoDBName == "" {
+		panic("MongoDB configuration is missing in environment variables")
+	}
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().
-		ApplyURI("mongodb+srv://lepi1:rizkidh123@cluster0.kfcww.mongodb.net/?appName=Cluster0").
+		ApplyURI(mongoURI).
 		SetServerAPIOptions(serverAPI).
 		SetMaxPoolSize(20).                 // Maksimum 20 koneksi dalam pool
 		SetMinPoolSize(5).                  // Minimal 5 koneksi dalam pool
+		SetMaxConnecting(10).               // Limit concurrent new connections
 		SetConnectTimeout(10 * time.Second) // Timeout saat koneksi dibuat
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -46,5 +55,6 @@ func MongoConnect() *mongo.Client {
 
 func MongoCollection(coll string) *mongo.Collection {
 	client := MongoConnect() // Gunakan koneksi yang sudah ada
-	return client.Database("gotrial").Collection(coll)
+	mongoDBName := os.Getenv("MONGO_DB_NAME")
+	return client.Database(mongoDBName).Collection(coll)
 }
